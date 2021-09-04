93a46b9b9b9c74bd072a679b5a7098b29188e4d6
hyperxpizza <wojtek.frackowski@gmail.com>
2021-09-04 15:25:17 +0200

    generate Comment files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_04_065115) do
+ActiveRecord::Schema.define(version: 2021_09_04_132043) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.string "email"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -20,4 +30,5 @@ ActiveRecord::Schema.define(version: 2021_09_04_065115) do
     t.string "slug"
   end
 
+  add_foreign_key "comments", "articles"
 end
